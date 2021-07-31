4648895c7e7d440526b24a01f62a7bcb3a3f394f
bsgribtmfu <bsgribtmfu@mailfence.com>
2021-07-31 23:34:08 +0200

    fixed the rendering logic of content(!) components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import { tripInfo } from './view/trip-info.js';
 import { navigation } from './view/navigation.js';
 import { filters } from './view/filters.js';
 import { tripSort } from './view/trip-sort.js';
-import { point } from './view/point.js';
-import { eventsList } from './view/events-list.js';
+import { event } from './view/event.js';
 import { editForm } from './view/edit-form.js';
 import { addForm } from './view/add-form.js';
 
@@ -16,20 +15,18 @@ const render = (element, position, template) => {
   element.insertAdjacentHTML(position, template);
 };
 
+const points = new Array(3).fill();
+
+const eventsList = () => (
+  `<ul class="trip-events__list">
+    <li class='trip-events__item'>${editForm()}</li>
+    <li class='trip-events__item'>${addForm()}</li>
+    ${points.map(() => `<li class='trip-events__item'>${event()}</li>`).join('')}
+  </ul>`
+);
+
 render(tripMain, 'afterbegin', tripInfo());
 render(tripNavigation, 'beforeend', navigation());
 render(tripControlsFilters, 'beforeend', filters());
 render(tripEvents, 'beforeend', tripSort());
 render(tripEvents, 'beforeend', eventsList());
-
-const listEvents = document.querySelector('.trip-events__list');
-const points = new Array(4).fill();
-
-points.forEach((_, index) => {
-  if(index === 0) {
-    render(listEvents, 'beforeend', `<li class='trip-events__item'>${editForm()}</li>`);
-    render(listEvents, 'beforeend', `<li class='trip-events__item'>${addForm()}</li>`);
-  } else {
-    render(listEvents, 'beforeend', `<li class='trip-events__item'>${point()}</li>`);
-  }
-});
