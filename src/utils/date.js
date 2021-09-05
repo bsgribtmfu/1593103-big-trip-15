7cd71f8447e8525e9f2ff6d21bb0b9c86a9cad7d
bsgribtmfu <bsgribtmfu@mailfence.com>
2021-09-06 01:33:37 +0200

    changed the function of calculating the time of one event, added date picker functionality with Flatpickr

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,5 @@
+import { getFirstLetter } from './common.js';
+
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 
@@ -13,9 +15,25 @@ const humanizeEventDate = (date, format) => dayjs(date).format(format);
 
 const getDiffDuration = (a, b) => getDiff(a.date_from, a.date_to) - getDiff(b.date_from, b.date_to);
 
+const humanizeEventDurationDate = (date) => {
+  let resultDateDiff = '';
+
+  Object
+    .keys(date)
+    .forEach((key) => {
+      if(date[key] !== 0 && key !== 'seconds' && key !== 'milliseconds') {
+        resultDateDiff += `${date[key]}${getFirstLetter(key)} `;
+      }
+    });
+
+  return resultDateDiff;
+};
+
 export {
   sortByDate,
   getDuration,
   humanizeEventDate,
-  getDiffDuration
+  getDiffDuration,
+  getDiff,
+  humanizeEventDurationDate
 };
